10ecebf6eae2ebd94fadc7c0cb5d637094bd69af
PRIYUSH KHOBRAGADE <124695270+Priyush02K@users.noreply.github.com>
2023-06-23 17:25:37 +0530

    Create Code1.dart

---
+++ Code1.dart
@@ -0,0 +1,13 @@
+void main() {
+  var somev1='10'; 
+  final somev2= '20';
+  final some=DateTime.now();
+  const somev3='30';
+  
+  print(somev1);
+    print(somev2);
+  print(somev3);
+
+    print(some);
+
+}
